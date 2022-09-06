473515fdc415cc0cf877d5def7c8f38205547148
Ricardo Vilela <vilelaricardo91@gmail.com>
2022-09-06 11:28:49 -0300

    add container; fix readme; fix tittle;

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const bodyParser = require('body-parser');
-
 const express = require('express');
 
 const app = express();
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 const PORT = 3000;
 
