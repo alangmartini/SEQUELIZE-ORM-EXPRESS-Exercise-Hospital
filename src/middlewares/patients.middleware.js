745cc621d90decc3f0cb1a18ebb852f71fefef2c
gmaralan <gmartinialan@gmail.com>
2023-03-27 15:32:51 -0400

    chore: validatePatient middleware skeleton

--- src/middlewares/patients.middleware.js
+++ src/middlewares/patients.middleware.js
@@ -1,2 +1,9 @@
-const JOI = require('joi');
+const patientSchema = require('./validations/patient.schema');
 
+const validatePatient = async (req, res, next) => {
+
+};
+
+module.exports = {
+  validatePatient,
+};
